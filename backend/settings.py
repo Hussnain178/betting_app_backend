370fd19b8b8f855824115fb57576208231502b2a
Hussnain <hussnain178.hk@gmail.com>
2025-07-14 18:12:00 +0500

    project commit

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret_key")
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.auth',
